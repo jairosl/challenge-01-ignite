1626f02751f4e2cf6b3bb97b42bf61c260a97f81
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-10-11 21:11:21 -0300

    feat: create Component taskList

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') return;
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 100),
+      isComplete: false,
+      title: newTaskTitle,
+    }
+
+    setTasks([...tasks, newTask]);
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const ToggleCompleteTask = tasks.map(task => {
+      if(task.id === id ) {
+        return {...task, isComplete: !task.isComplete}
+      }
+      return task;
+    });
+
+    setTasks(ToggleCompleteTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id)
+    setTasks(removeTask);
   }
 
   return (
